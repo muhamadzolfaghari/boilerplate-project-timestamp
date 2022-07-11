eb57d4c1c9788a57383e6acda905304b6e4b8d65
muhamad.zolfaghari <you@example.com>
2022-07-11 22:38:55 +0430

    The .env file was added to project and the challenge was finished.

--- index.js
+++ index.js
@@ -2,31 +2,53 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
+const isNumber = require("is-number");
+require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-const cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
-
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api/:date?", (req, res) => {
+  /** @type Date */
+  let date;
+  const paramsDate = req.params.date;
 
+  if (!paramsDate) {
+    date = new Date();
+  } else {
+    if (isNumber(paramsDate)) {
+      date = new Date();
+      date.setTime(+paramsDate);
+    } else {
+      date = new Date(paramsDate);
+    }
+
+    if (!date.getTime()) {
+      return res.json({ error: "Invalid Date" });
+    }
+  }
+
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
